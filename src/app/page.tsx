41f8145b677ddf3bd7e7881cbcf7946a8907fce9
Ketul Shah <ketu001in@gmail.com>
2026-07-22 19:44:01 +0530

    Return trailer player to the idle thumbnail+quote view when playback ends
    onEnded on the playing <video> now flips videoPlaying back to false,
    so finishing the trailer re-renders the idle state (thumbnail, quote
    overlay, Play button) instead of leaving the finished player sitting
    there.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -353,6 +353,7 @@ export default function Home() {
                   controls
                   autoPlay
                   playsInline
+                  onEnded={() => setVideoPlaying(false)}
                 />
                 <button
                   onClick={() => setVideoPlaying(false)}
